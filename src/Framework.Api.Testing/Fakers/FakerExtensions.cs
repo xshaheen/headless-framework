fb814e5e96312df266a9ebbf80e3df0e92cc77d8
Shaheen <mxshaheen@gmail.com>
2024-08-04 22:07:47 +0300

    Add initial support for Framework.Api.Testing

---
+++ src/Framework.Api.Testing/Fakers/FakerExtensions.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace Bogus;
+
+public static class FakerExtensions
+{
+    private static readonly bool?[] _Values = [true, false, null];
+
+    public static bool? NullableBool(this Faker faker)
+    {
+        return faker.PickRandom(_Values);
+    }
+}
