e5ba13187b0f68e31ceb0dc16983952f9b7c6422
Shaheen <mxshaheen@gmail.com>
2024-08-06 23:48:26 +0300

    Add specification project

---
+++ src/Framework.BuildingBlocks.Specifications/AnySpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Framework.BuildingBlocks.Specifications;
+
+/// <summary>Represents the specification that can be satisfied by the given object in any circumstance.</summary>
+/// <typeparam name="T">The type of the object to which the specification is applied.</typeparam>
+public sealed class AnySpecification<T> : Specification<T>
+{
+    public override Expression<Func<T, bool>> ToExpression()
+    {
+        return o => true;
+    }
+}
