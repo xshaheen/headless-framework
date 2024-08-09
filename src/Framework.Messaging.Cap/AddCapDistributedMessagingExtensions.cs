9c7bd20b8ab60c01cc891df6fd5190830a4b2502
Shaheen <mxshaheen@gmail.com>
2024-08-09 23:01:01 +0300

    Add convert DistributedMessageHandlers to CAP subscribe

---
+++ src/Framework.Messaging.Cap/AddCapDistributedMessagingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.Messaging.Cap;
+
+public static class AddCapDistributedMessagingExtensions
+{
+    public static void AddCapDistributedMessaging(this IServiceCollection services)
+    {
+        services.AddSingleton<IDistributedMessagePublisher, CapDistributedMessagePublisher>();
+        services.AddSingleton(CapDistributedMessageHandlerFactory.Create());
+    }
+}
