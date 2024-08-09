462dc52777fe534688b448097d543b89049d8b95
Shaheen <mxshaheen@gmail.com>
2024-08-09 23:05:56 +0300

    Add Cap itself in the AddCapDistributedMessaging extensions

--- src/Framework.Messaging.Cap/AddCapDistributedMessagingExtensions.cs
+++ src/Framework.Messaging.Cap/AddCapDistributedMessagingExtensions.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DotNetCore.CAP;
+using Framework.BuildingBlocks.Constants;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Framework.Messaging.Cap;
 
 public static class AddCapDistributedMessagingExtensions
 {
-    public static void AddCapDistributedMessaging(this IServiceCollection services)
+    public static void AddCapDistributedMessaging(this IServiceCollection services, Action<CapOptions> setupAction)
     {
         services.AddSingleton<IDistributedMessagePublisher, CapDistributedMessagePublisher>();
         services.AddSingleton(CapDistributedMessageHandlerFactory.Create());
+
+        services.AddCap(capOptions =>
+        {
+            PlatformJsonConstants.ConfigureInternalJsonOptions(capOptions.JsonSerializerOptions);
+
+            setupAction.Invoke(capOptions);
+        });
     }
 }
