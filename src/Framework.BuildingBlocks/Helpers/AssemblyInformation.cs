a229900c785a23e6429096fb5f8c1ca8a6fa92e8
Shaheen <mxshaheen@gmail.com>
2024-08-10 00:38:50 +0300

    Make AssemblyInformation public

---
+++ src/Framework.BuildingBlocks/Helpers/AssemblyInformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Framework.BuildingBlocks.Helpers;
+
+public sealed record AssemblyInformation(string? Product, string? Description, string? Version)
+{
+    public static readonly AssemblyInformation Current = new(typeof(AssemblyInformation).Assembly);
+
+    public static readonly AssemblyInformation Entry = new(Assembly.GetEntryAssembly()!);
+
+    private AssemblyInformation(Assembly assembly)
+        : this(
+            Product: assembly.GetAssemblyProduct(),
+            Description: assembly.GetAssemblyDescription(),
+            Version: assembly.GetAssemblyVersion()
+        ) { }
+}
