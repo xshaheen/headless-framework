33ed29eafaf68877e03b679ed1801b50bd0fcd4b
Shaheen <mxshaheen@gmail.com>
2024-08-24 14:03:55 +0300

    Handle Azure blobs page correctly

---
+++ src/Framework.Blobs.Azure/Internals/AzureNextPageResult.cs
@@ -0,0 +1,19 @@
+﻿namespace Framework.Blobs.Azure.Internals;
+
+public sealed class AzureNextPageResult : INextPageResult
+{
+    public required bool Success { get; init; }
+
+    public required bool HasMore { get; init; }
+
+    public required IReadOnlyCollection<BlobSpecification> Blobs { get; init; }
+
+    public required IReadOnlyCollection<BlobSpecification> ExtraLoadedBlobs { get; init; }
+
+    public required string? ContinuationToken { get; init; }
+
+    public required Func<AzureNextPageResult, Task<AzureNextPageResult>>? AzureNextPageFunc { get; init; }
+
+    public Func<PagedFileListResult, Task<INextPageResult>>? NextPageFunc =>
+        AzureNextPageFunc is null ? null : async result => await AzureNextPageFunc.Invoke(this);
+}
