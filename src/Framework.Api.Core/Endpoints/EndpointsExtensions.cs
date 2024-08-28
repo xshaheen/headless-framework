3e202fd71ce3286c13244a8c1c57c60a30a02c53
Shaheen <mxshaheen@gmail.com>
2024-08-29 02:11:11 +0300

    Add RedirectHosts extension

---
+++ src/Framework.Api.Core/Endpoints/EndpointsExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Flurl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Framework.Api.Core.Endpoints;
+
+public static class EndpointsExtensions
+{
+    public static void RedirectHosts(this WebApplication app, string mainHost, string[] redirectHosts)
+    {
+        if (redirectHosts is not { Length: > 0 })
+        {
+            return;
+        }
+
+        app.MapGet(
+                pattern: "{*path}",
+                handler: (HttpContext context, string? path) =>
+                {
+                    var location = mainHost;
+
+                    if (path is not null)
+                    {
+                        location = Url.Combine(location, path);
+                    }
+
+                    context.Response.Redirect(location, permanent: true);
+
+                    return ValueTask.CompletedTask;
+                }
+            )
+            .RequireHost(redirectHosts);
+    }
+}
