fac551791aca31d81850505ec98ac99fb7a811e9
Shaheen <mxshaheen@gmail.com>
2024-09-27 14:13:27 +0300

    Add default primitive contract view & requests

---
+++ src/Framework.Api.Core/Contracts/PhoneNumbers/PhoneNumberView.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+using Framework.Kernel.Primitives;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+// ReSharper disable once CheckNamespace
+namespace Framework.Api.Core.Contracts;
+
+public sealed record PhoneNumberView(int Code, string Number)
+{
+    [return: NotNullIfNotNull(nameof(operand))]
+    public static PhoneNumberView? FromPhoneNumber(PhoneNumber? operand) => operand;
+
+    [return: NotNullIfNotNull(nameof(operand))]
+    public static implicit operator PhoneNumberView?(PhoneNumber? operand)
+    {
+        return operand is null ? null : new(operand.CountryCode, operand.Number);
+    }
+}
