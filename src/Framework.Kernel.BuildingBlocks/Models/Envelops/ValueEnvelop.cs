5fbf8a0dddb45ebcbcc2f3be3b7ecd4af405cf15
Shaheen <mxshaheen@gmail.com>
2024-10-04 22:40:07 +0300

    Add operations envelop

--- src/Framework.Kernel.BuildingBlocks/Models/Envelops/ValueEnvelop.cs
+++ src/Framework.Kernel.BuildingBlocks/Models/Envelops/ValueEnvelop.cs
@@ -1,12 +1,10 @@
 ﻿// Copyright (c) Mahmoud Shaheen, 2024. All rights reserved
 
-#pragma warning disable IDE0130 // Namespace does not match folder structure
+#pragma warning disable CA2225, IDE0130 // Namespace does not match folder structure
 // ReSharper disable once CheckNamespace
 namespace Framework.Kernel.Primitives;
 
 public sealed record ValueEnvelop<T>(T Data)
 {
     public static implicit operator ValueEnvelop<T>(T operand) => new(operand);
-
-    public ValueEnvelop<T> FromT() => this;
 }
