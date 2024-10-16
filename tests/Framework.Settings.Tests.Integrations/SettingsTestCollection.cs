0448f23b9094a5575097e6ffc64e5431c5c0520b
Shaheen <mxshaheen@gmail.com>
2024-10-16 15:59:25 +0300

    Push initial test data
    Signed-off-by: Mahmoud Shaheen <a.mshaheen@dev.thiqah.sa>
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

---
+++ tests/Framework.Settings.Tests.Integrations/SettingsTestCollection.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Mahmoud Shaheen, 2024. All rights reserved
+
+namespace Framework.Settings.Tests.Integrations;
+
+[CollectionDefinition(nameof(SettingsTestCollection))]
+public class SettingsTestCollection : ICollectionFixture<SettingsTestFixture>;
