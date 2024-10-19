2c46d033e8f04a4d9bc689beb4eb594f1ba7bc1b
Shaheen <mxshaheen@gmail.com>
2024-10-19 17:20:09 +0300

    Add api key schema
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

---
+++ src/Framework.Api/Identity/Authentication/ApiKey/ApiKeyAuthenticationSchemeOptions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Mahmoud Shaheen, 2024. All rights reserved
+
+using Framework.Kernel.BuildingBlocks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Framework.Api.Identity.Authentication.ApiKey;
+
+[PublicAPI]
+public sealed class ApiKeyAuthenticationSchemeOptions : AuthenticationSchemeOptions
+{
+    public const string DefaultScheme = "API Key Authentication";
+    public const string ParamName = "api_key";
+    public const string HeaderName = HttpHeaderNames.ApiKey;
+
+    public string ApiKeyParamName { get; set; } = ParamName;
+
+    public string ApiKeyHeaderName { get; set; } = HeaderName;
+}
