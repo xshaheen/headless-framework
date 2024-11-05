754cebe8cd9ac41c9fb41b5da52ec23ade0db8ec
Shaheen <mxshaheen@gmail.com>
2024-11-05 16:09:59 +0300

    Add ef migration helper project
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

---
+++ demo/Framework.EntityFramework.Migration.Startup/Program.cs
@@ -0,0 +1,24 @@
+﻿using Framework.Api;
+using Framework.Caching;
+using Framework.ResourceLocks.Local;
+using Framework.Settings;
+using Framework.Settings.Storage.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+// To add a migration use:
+// dotnet ef migrations add InitialMigration -s .\demo\Framework.EntityFramework.Migration.Startup -o .\Migrations -p .\src\Framework.Settings.Storage.EntityFramework
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddFrameworkApiServices();
+builder.Services.AddInMemoryCache();
+builder.Services.AddLocalResourceLock();
+
+builder
+    .Services.AddSettingsManagementCore()
+    .AddSettingsManagementEntityFrameworkStorage(options =>
+    {
+        options.UseSqlite("Data Source=Settings.db");
+    });
+
+await builder.Build().RunAsync();
