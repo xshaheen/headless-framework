b9320e319a2a21139b3f6c1319854c1a723e8c48
Shaheen <mxshaheen@gmail.com>
2024-11-13 15:42:34 +0300

    Fix: ssh blobs test
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

--- tests/Framework.Blobs.SshNet.Tests.Integrations/SshBlobStorageTests.cs
+++ tests/Framework.Blobs.SshNet.Tests.Integrations/SshBlobStorageTests.cs
@@ -2,37 +2,23 @@ using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using Framework.Blobs;
 using Framework.Blobs.SshNet;
-using Framework.Blobs.Tests.Harness;
 using Microsoft.Extensions.Options;
 
 namespace Tests;
 
 [Collection("SshBlobStorageIntegrationTests")]
-public sealed class SshBlobStorageTests : FileStorageTestsBase, IAsyncLifetime
+public sealed class SshBlobStorageTests(ITestOutputHelper output) : FileStorageTestsBase(output), IAsyncLifetime
 {
-    private readonly IContainer _sftpContainer;
-    private readonly IContainer _awaiterContainer;
-
-    public SshBlobStorageTests(ITestOutputHelper output)
-        : base(output)
-    {
-        _sftpContainer = new ContainerBuilder()
-            .WithImage("atmoz/sftp:latest")
-            .WithPortBinding(2222, 22)
-            .WithCommand("framework:password:::storage")
-            .Build();
-
-        _awaiterContainer = new ContainerBuilder()
-            .DependsOn(_sftpContainer)
-            .WithImage("andrewlock/wait-for-dependencies:latest")
-            .WithCommand("sftp:22")
-            .Build();
-    }
+    private readonly IContainer _sftpContainer = new ContainerBuilder()
+        .WithImage("atmoz/sftp:latest")
+        .WithPortBinding(2222, 22)
+        .WithCommand("framework:password:::storage")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(22))
+        .Build();
 
     public async Task InitializeAsync()
     {
         await _sftpContainer.StartAsync();
-        await _awaiterContainer.StartAsync();
     }
 
     public async Task DisposeAsync()
@@ -52,5 +38,11 @@ public sealed class SshBlobStorageTests : FileStorageTestsBase, IAsyncLifetime
     public override Task CanGetEmptyFileListOnMissingDirectoryAsync()
     {
         return base.CanGetEmptyFileListOnMissingDirectoryAsync();
+    }
+
+    [Fact]
+    public override Task CanGetFileListForSingleFolderAsync()
+    {
+        return base.CanGetFileListForSingleFolderAsync();
     }
 }
