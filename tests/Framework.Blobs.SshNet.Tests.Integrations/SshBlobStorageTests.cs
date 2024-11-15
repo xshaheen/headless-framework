9638a118f4aa57907fb5df4f4a4b415d6962f346
Shaheen <mxshaheen@gmail.com>
2024-11-15 20:04:39 +0200

    Add blob storage tests
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

--- tests/Framework.Blobs.SshNet.Tests.Integrations/SshBlobStorageTests.cs
+++ tests/Framework.Blobs.SshNet.Tests.Integrations/SshBlobStorageTests.cs
@@ -37,6 +37,32 @@ public sealed class SshBlobStorageTests(ITestOutputHelper output) : FileStorageT
     }
 
     [Fact]
+    public void CanCreateSshNetFileStorageWithoutConnectionStringPassword()
+    {
+        // given
+        var options = new SshBlobStorageSettings { ConnectionString = "sftp://framework@localhost:2222" };
+        var optionsWrapper = new OptionsWrapper<SshBlobStorageSettings>(options);
+
+        // when
+        using var storage = new SshBlobStorage(optionsWrapper);
+    }
+
+    [Fact]
+    public void CanCreateSshNetFileStorageWithoutProxyPassword()
+    {
+        // given
+        var options = new SshBlobStorageSettings
+        {
+            ConnectionString = "sftp://username@host",
+            Proxy = "proxy://username@host",
+        };
+        var optionsWrapper = new OptionsWrapper<SshBlobStorageSettings>(options);
+
+        // when
+        using var storage = new SshBlobStorage(optionsWrapper);
+    }
+
+    [Fact]
     public override Task CanGetEmptyFileListOnMissingDirectoryAsync()
     {
         return base.CanGetEmptyFileListOnMissingDirectoryAsync();
@@ -58,5 +84,135 @@ public sealed class SshBlobStorageTests(ITestOutputHelper output) : FileStorageT
     public override Task CanGetPagedFileListForSingleFolderAsync()
     {
         return base.CanGetPagedFileListForSingleFolderAsync();
+    }
+
+    [Fact]
+    public override Task CanGetFileInfoAsync()
+    {
+        return base.CanGetFileInfoAsync();
+    }
+
+    [Fact]
+    public override Task CanGetNonExistentFileInfoAsync()
+    {
+        return base.CanGetNonExistentFileInfoAsync();
+    }
+
+    [Fact]
+    public override Task CanManageFilesAsync()
+    {
+        return base.CanManageFilesAsync();
+    }
+
+    [Fact]
+    public override Task CanRenameFilesAsync()
+    {
+        return base.CanRenameFilesAsync();
+    }
+
+    [Fact(Skip = "Doesn't work well with SFTP")]
+    public override Task CanConcurrentlyManageFilesAsync()
+    {
+        return base.CanConcurrentlyManageFilesAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteEntireFolderAsync()
+    {
+        return base.CanDeleteEntireFolderAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteEntireFolderWithWildcardAsync()
+    {
+        return base.CanDeleteEntireFolderWithWildcardAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteFolderWithMultiFolderWildcardsAsync()
+    {
+        return base.CanDeleteFolderWithMultiFolderWildcardsAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteSpecificFilesAsync()
+    {
+        return base.CanDeleteSpecificFilesAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteNestedFolderAsync()
+    {
+        return base.CanDeleteNestedFolderAsync();
+    }
+
+    [Fact]
+    public override Task CanDeleteSpecificFilesInNestedFolderAsync()
+    {
+        return base.CanDeleteSpecificFilesInNestedFolderAsync();
+    }
+
+    [Fact]
+    public override Task CanRoundTripSeekableStreamAsync()
+    {
+        return base.CanRoundTripSeekableStreamAsync();
+    }
+
+    [Fact]
+    public override Task WillRespectStreamOffsetAsync()
+    {
+        return base.WillRespectStreamOffsetAsync();
+    }
+
+    [Fact]
+    public override Task CanSaveOverExistingStoredContent()
+    {
+        return base.CanSaveOverExistingStoredContent();
+    }
+
+    [Fact]
+    public async Task WillNotReturnDirectoryInGetPagedFileListAsync()
+    {
+        var storage = GetStorage();
+
+        await ResetAsync(storage);
+
+        using (storage)
+        {
+            var container = GetContainer();
+
+            var result = await storage.GetPagedListAsync(container);
+            result.HasMore.Should().BeFalse();
+            result.Blobs.Should().BeEmpty();
+            (await result.NextPageAsync()).Should().BeFalse();
+            result.HasMore.Should().BeFalse();
+            result.Blobs.Should().BeEmpty();
+
+            const string directory = "EmptyDirectory";
+            var client = storage is SshBlobStorage sshStorage ? await sshStorage.GetClientAsync() : null;
+            client.Should().NotBeNull();
+
+            await client!.CreateDirectoryAsync($"storage/{directory}");
+
+            result = await storage.GetPagedListAsync(container);
+            result.HasMore.Should().BeFalse();
+            result.Blobs.Should().BeEmpty();
+            (await result.NextPageAsync()).Should().BeFalse();
+            result.HasMore.Should().BeFalse();
+            result.Blobs.Should().BeEmpty();
+
+            // Ensure the directory will not be returned via get file info
+            var info = await storage.GetBlobInfoAsync(container, directory);
+            info.Should().BeNull();
+
+            // Ensure delete files can remove all files including fake folders
+            var count = await storage.DeleteAllAsync(container, "*");
+
+            // Assert folder was removed by Delete Files
+            count.Should().Be(1);
+            (await client.ExistsAsync($"storage/{directory}")).Should().BeFalse();
+
+            (await storage.GetBlobInfoAsync(container, directory)).Should().BeNull();
+        }
     }
 }
