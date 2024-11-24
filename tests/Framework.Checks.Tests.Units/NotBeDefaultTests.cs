0741f9f72db1369de76120b83b7470f801ec5e9a
Shaheen <mxshaheen@gmail.com>
2024-11-24 18:39:06 +0200

    Add check project tests
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

---
+++ tests/Framework.Checks.Tests.Units/NotBeDefaultTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Framework.Checks;
+
+namespace Tests;
+
+public sealed class NotBeDefaultTests
+{
+    [Fact]
+    public void should_throw_argument_exception_when_pass_default_value()
+    {
+        var zero = 0;
+        Assert.Throws<ArgumentException>(nameof(zero), () => Argument.IsNotDefault(zero));
+        int? nullableZero = 0;
+        Assert.Throws<ArgumentException>(nameof(nullableZero), () => Argument.IsNotDefault(nullableZero));
+        var emptyGuid = Guid.Empty;
+        Assert.Throws<ArgumentException>(nameof(emptyGuid), () => Argument.IsNotDefault(emptyGuid));
+        var defaultDateTime = default(DateTime);
+        Assert.Throws<ArgumentException>(nameof(defaultDateTime), () => Argument.IsNotDefault(defaultDateTime));
+    }
+
+    [Fact]
+    public void should_do_nothing_given_non_default_value_and_return_expected_value()
+    {
+        Argument.IsNotDefault(1).Should().Be(1);
+        Argument.IsNotDefault((int?)-1).Should().Be((int?)-1);
+        Argument.IsNotDefault((int?)1).Should().Be((int?)1);
+        Argument.IsNotDefault((int?)null).Should().Be(null);
+        Argument.IsNotDefault(new DateTime(2000, 1, 1)).Should().Be(new DateTime(2000, 1, 1));
+        var guid = Guid.NewGuid();
+        Argument.IsNotDefault(guid).Should().Be(guid);
+    }
+}
