7d582645b87cd75d509bac5407cba2061b467361
Shaheen <mxshaheen@gmail.com>
2024-12-09 16:06:17 +0300

    Use Framework.Defaults to automatically set default ms build configs

--- src/Framework.Generator.Primitives/Emitter.cs
+++ src/Framework.Generator.Primitives/Emitter.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
+using System.Collections.Immutable;
 using Framework.Generator.Primitives.Extensions;
 using Framework.Generator.Primitives.Helpers;
 using Framework.Generator.Primitives.Models;
