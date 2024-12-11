d92bf96b2a3aee830273b43eeed5405ad6bb6dd7
Sleem Mostafa <105351178+SleemMostafa@users.noreply.github.com>
2024-12-11 11:47:30 +0200

    Feat: do unit testing for specification module (#11)
    * Feat: do unit testing for specification module
    
    Signed-off-by: Sleem Mostafa <Sleem.Mostafa@cybral.com>
    
    * do testing for SpecificationExtensions
    
    Signed-off-by: Sleem Mostafa <Sleem.Mostafa@cybral.com>
    
    ---------
    
    Signed-off-by: Sleem Mostafa <Sleem.Mostafa@cybral.com>
    Co-authored-by: Sleem Mostafa <Sleem.Mostafa@cybral.com>

---
+++ tests/Framework.Specifications.Tests.Units/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+namespace Tests.Models;
+
+public sealed class Product
+{
+    public string Name { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = default!;
+
+    public int Stock { get; set; }
+
+    public Color Color { get; set; }
+
+    public bool Available { get; set; }
+}
+
+public enum Color
+{
+    Default,
+    Red,
+    Green,
+    Black,
+    White,
+}
