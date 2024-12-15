bf32b20bda994b5bb31635dc376cc1db594da702
Shaheen <mxshaheen@gmail.com>
2024-12-15 12:20:32 +0300

    Add new RespectNullableAnnotations & RespectRequiredConstructorParameters JSON options

--- src/Framework.Api.MinimalApi/Options/ConfigureMinimalApiJsonOptions.cs
+++ src/Framework.Api.MinimalApi/Options/ConfigureMinimalApiJsonOptions.cs
@@ -1,11 +1,10 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
-using System.Text.Encodings.Web;
+using Framework.BuildingBlocks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using NetTopologySuite.IO.Converters;
 
 namespace Framework.Api.MinimalApi.Options;
 
@@ -14,24 +13,9 @@ public sealed class ConfigureMinimalApiJsonOptions(IWebHostEnvironment environme
 {
     public void Configure(JsonOptions options)
     {
-        var jsonOptions = options.SerializerOptions;
+        FrameworkJsonConstants.ConfigureWebJsonOptions(options.SerializerOptions);
 
-        jsonOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-        jsonOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
-        jsonOptions.NumberHandling = JsonNumberHandling.AllowReadingFromString;
-        jsonOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        jsonOptions.PreferredObjectCreationHandling = JsonObjectCreationHandling.Replace;
-        jsonOptions.UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode;
-        jsonOptions.UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip;
-        jsonOptions.IgnoreReadOnlyProperties = false;
-        jsonOptions.PropertyNameCaseInsensitive = false;
         // Pretty print the JSON in development for easier debugging.
-        jsonOptions.WriteIndented = environment.IsDevelopmentOrTest();
-
-        jsonOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        jsonOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-
-        jsonOptions.Converters.Add(new GeoJsonConverterFactory());
-        jsonOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, allowIntegerValues: false));
+        options.SerializerOptions.WriteIndented = environment.IsDevelopmentOrTest();
     }
 }
--- src/Framework.Api.Mvc/Options/ConfigureMvcJsonOptions.cs
+++ src/Framework.Api.Mvc/Options/ConfigureMvcJsonOptions.cs
@@ -1,37 +1,21 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
-using System.Text.Encodings.Web;
+using Framework.BuildingBlocks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using NetTopologySuite.IO.Converters;
 
 namespace Framework.Api.Mvc.Options;
 
 [PublicAPI]
-public sealed class ConfigureMvcJsonOptions(IWebHostEnvironment webHostEnvironment) : IConfigureOptions<JsonOptions>
+public sealed class ConfigureMvcJsonOptions(IWebHostEnvironment environment) : IConfigureOptions<JsonOptions>
 {
     public void Configure(JsonOptions options)
     {
-        var jsonOptions = options.JsonSerializerOptions;
+        FrameworkJsonConstants.ConfigureWebJsonOptions(options.JsonSerializerOptions);
 
-        jsonOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-        jsonOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
-        jsonOptions.NumberHandling = JsonNumberHandling.AllowReadingFromString;
-        jsonOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        jsonOptions.PreferredObjectCreationHandling = JsonObjectCreationHandling.Replace;
-        jsonOptions.UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode;
-        jsonOptions.UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip;
-        jsonOptions.IgnoreReadOnlyProperties = false;
-        jsonOptions.PropertyNameCaseInsensitive = false;
         // Pretty print the JSON in development for easier debugging.
-        jsonOptions.WriteIndented = webHostEnvironment.IsDevelopmentOrTest();
-
-        jsonOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        jsonOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-
-        jsonOptions.Converters.Add(new GeoJsonConverterFactory());
-        jsonOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, allowIntegerValues: false));
+        options.JsonSerializerOptions.WriteIndented = environment.IsDevelopmentOrTest();
     }
 }
