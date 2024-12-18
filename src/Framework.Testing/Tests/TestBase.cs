a7b9d67c8a191fa390ba4e4015b616c8288c6fbe
Shaheen <mxshaheen@gmail.com>
2024-12-18 16:09:00 +0300

    Add test base class

---
+++ src/Framework.Testing/Tests/TestBase.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Meziantou.Extensions.Logging.Xunit;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Framework.Testing.Tests;
+
+public abstract class TestBase : IDisposable
+{
+    private bool _disposed;
+
+    protected ILoggerFactory LoggerFactory { get; }
+
+    protected ILoggerProvider LoggerProvider { get; }
+
+    protected ILogger Logger { get; }
+
+    protected TestBase(ITestOutputHelper output)
+    {
+        LoggerFactory = new LoggerFactory();
+
+        LoggerProvider = new XUnitLoggerProvider(
+            output,
+            new XUnitLoggerOptions
+            {
+                IncludeLogLevel = true,
+                IncludeScopes = true,
+                IncludeCategory = true,
+            }
+        );
+
+        LoggerFactory.AddProvider(LoggerProvider);
+
+        Logger = LoggerFactory.CreateLogger(GetType());
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            LoggerFactory.Dispose();
+            LoggerProvider.Dispose();
+        }
+
+        _disposed = true;
+    }
+}
