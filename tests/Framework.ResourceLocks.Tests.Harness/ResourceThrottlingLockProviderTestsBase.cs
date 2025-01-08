e1d06a159ea43cedd5fa0573a2decd86dfae7b7a
Shaheen <mxshaheen@gmail.com>
2025-01-08 15:21:34 +0300

    Fix: throttling lock

--- tests/Framework.ResourceLocks.Tests.Harness/ResourceThrottlingLockProviderTestsBase.cs
+++ tests/Framework.ResourceLocks.Tests.Harness/ResourceThrottlingLockProviderTestsBase.cs
@@ -12,9 +12,9 @@ public abstract class ResourceThrottlingLockProviderTestsBase(ITestOutputHelper 
 
     public virtual async Task should_throttle_calls_async()
     {
-        const int allowedLocks = 1000;
+        const int allowedLocks = 25;
         var resource = Guid.NewGuid().ToString("N")[..10];
-        var period = TimeSpan.FromSeconds(3);
+        var period = TimeSpan.FromSeconds(2);
 
         var lockProvider = GetLockProvider(allowedLocks, period);
         // Sleep until start of throttling period
@@ -25,14 +25,14 @@ public abstract class ResourceThrottlingLockProviderTestsBase(ITestOutputHelper 
         await _AssertCannotAcquireMore(lockProvider, resource);
         // Try to acquire a lock while all allowed locks are still acquired but with
         // a acquireTimeout greater than the remaining period to reset the throttling
-        await _AssertCanAcquireWithGreaterWait(lockProvider, resource);
+        await _AssertCanAcquireWithGreaterWait(lockProvider, resource, TimeSpan.FromSeconds(2.5));
     }
 
     public virtual async Task should_throttle_concurrent_calls_async()
     {
-        const int allowedLocks = 1000;
+        const int allowedLocks = 25;
         var resource = Guid.NewGuid().ToString("N")[..10];
-        var period = TimeSpan.FromSeconds(3);
+        var period = TimeSpan.FromSeconds(2);
 
         var lockProvider = GetLockProvider(allowedLocks, period);
         // Sleep until start of throttling period
@@ -43,7 +43,7 @@ public abstract class ResourceThrottlingLockProviderTestsBase(ITestOutputHelper 
         await _AssertCannotAcquireMore(lockProvider, resource);
         // Try to acquire a lock while all allowed locks are still acquired but with
         // a acquireTimeout greater than the remaining period to reset the throttling
-        await _AssertCanAcquireWithGreaterWait(lockProvider, resource);
+        await _AssertCanAcquireWithGreaterWait(lockProvider, resource, TimeSpan.FromSeconds(2.5));
     }
 
     private static async Task _SleepUntilStartOfPeriod(TimeSpan period)
@@ -103,10 +103,10 @@ public abstract class ResourceThrottlingLockProviderTestsBase(ITestOutputHelper 
         result.Should().BeNull();
     }
 
-    private async Task _AssertCanAcquireWithGreaterWait(IResourceThrottlingLockProvider provider, string resource)
+    private async Task _AssertCanAcquireWithGreaterWait(IResourceThrottlingLockProvider provider, string resource, TimeSpan acquireTimeout)
     {
         var timestamp = Stopwatch.GetTimestamp();
-        var result = await provider.TryAcquireAsync(resource, acquireTimeout: TimeSpan.FromSeconds(2.5));
+        var result = await provider.TryAcquireAsync(resource, acquireTimeout: acquireTimeout);
         var elapsed = Stopwatch.GetElapsedTime(timestamp);
         Logger.LogInformation("Time to acquire lock: {Elapsed:g}", elapsed);
         result.Should().NotBeNull();
