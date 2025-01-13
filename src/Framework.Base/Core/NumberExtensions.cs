c176036c2b2559b6b2d8547a6504df26c466d161
Shaheen <mxshaheen@gmail.com>
2025-01-13 19:55:45 +0200

    Add clamp extension

---
+++ src/Framework.Base/Core/NumberExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace System;
+
+[PublicAPI]
+public static class NumberExtensions
+{
+    [SystemPure]
+    [JetBrainsPure]
+    public static int Clamp(this int value, int min, int max) => Math.Clamp(value, min, max);
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static long Clamp(this long value, long min, long max) => Math.Clamp(value, min, max);
+}
---
+++ src/Framework.Base/Core/TimeSpanExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace System;
+
+[PublicAPI]
+public static class TimeSpanExtensions
+{
+    [SystemPure]
+    [JetBrainsPure]
+    public static TimeSpan Clamp(this TimeSpan timeSpan, TimeSpan min, TimeSpan max)
+    {
+        return timeSpan < min ? min
+            : timeSpan > max ? max
+            : timeSpan;
+    }
+}
