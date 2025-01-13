03cc1ea201c145a1d622d4589d3a28fe46291f80
Shaheen <mxshaheen@gmail.com>
2025-01-13 19:57:37 +0200

    Revamp sqlite test fixture

--- tests/Framework.ResourceLocks.Sqlite.Tests.Integration/TestSetup/SqliteTestFixture.cs
+++ tests/Framework.ResourceLocks.Sqlite.Tests.Integration/TestSetup/SqliteTestFixture.cs
@@ -1,43 +1,37 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
 using Framework.Database.Sqlite;
-using Framework.ResourceLocks;
 using Framework.ResourceLocks.Sqlite;
-using Framework.ResourceLocks.Storage.RegularLocks;
+using Microsoft.Data.Sqlite;
 
 namespace Tests.TestSetup;
 
 [CollectionDefinition(nameof(SqliteTestFixture))]
 public sealed class SqliteTestFixture : IAsyncLifetime, IAsyncDisposable, ICollectionFixture<SqliteTestFixture>
 {
-    private readonly SqliteConnectionFactory _connectionFactory = new("DataSource=./ThrottlingResourceLocks.db");
+    public SqliteConnectionFactory ConnectionFactory { get; } = new("DataSource=./ThrottlingResourceLocks.db");
 
-    public IResourceLockStorage ResourceLockStorage { get; private set; } = null!;
+    public SqliteConnection? Connection { get; private set; }
 
-    public IThrottlingResourceLockStorage ThrottlingResourceLockStorage { get; private set; } = null!;
+    public SqliteResourceLockStorage LockStorage { get; private set; } = null!;
+
+    public SqliteThrottlingResourceLockStorage ThrottlingLockStorage { get; private set; } = null!;
 
     /// <summary>This runs before all the test run and Called just after the constructor</summary>
     async Task IAsyncLifetime.InitializeAsync()
     {
         // Create the resource lock table
-        var connection = await _connectionFactory.GetOpenConnectionAsync();
-        var resourceLockStorage = new SqliteResourceLockStorage(connection);
-        await resourceLockStorage.CreateTableAsync();
-        ResourceLockStorage = resourceLockStorage;
+        Connection = await ConnectionFactory.GetOpenConnectionAsync();
 
-        var throttlingResourceLockStorage = new SqliteThrottlingResourceLockStorage(connection, TimeProvider.System);
-        await throttlingResourceLockStorage.CreateTableAsync();
-        ThrottlingResourceLockStorage = throttlingResourceLockStorage;
+        LockStorage = new SqliteResourceLockStorage(Connection);
+        await LockStorage.CreateTableAsync();
+
+        ThrottlingLockStorage = new(Connection, TimeProvider.System);
+        await ThrottlingLockStorage.CreateTableAsync();
     }
 
     /// <summary>This runs after all the test run and Called before Dispose()</summary>
-    Task IAsyncLifetime.DisposeAsync()
-    {
-        return Task.CompletedTask;
-    }
+    Task IAsyncLifetime.DisposeAsync() => Task.CompletedTask;
 
-    public async ValueTask DisposeAsync()
-    {
-        await _connectionFactory.DisposeAsync();
-    }
+    public async ValueTask DisposeAsync() => await ConnectionFactory.DisposeAsync();
 }
