37a48a51874790cbebbaa45ef7df66d8b20b9909
Shaheen <mxshaheen@gmail.com>
2025-01-14 18:01:10 +0300

    Revamp

--- tests/Framework.Base.Tests.Unit/Collections/ConcurrentDictionaryExtensionsTests.cs
+++ tests/Framework.Base.Tests.Unit/Collections/ConcurrentDictionaryExtensionsTests.cs
@@ -18,33 +18,4 @@ public sealed class ConcurrentDictionaryExtensionsTests
         // then
         result.Should().Be(0);
     }
-
-    [Fact]
-    public void try_update_should_update_value_if_key_exists()
-    {
-        // given
-        var dictionary = new ConcurrentDictionary<string, int>(StringComparer.Ordinal);
-        dictionary.TryAdd("key", 42);
-
-        // when
-        var updated = dictionary.TryUpdate("key", (key, oldValue) => oldValue + 1);
-        var value = dictionary["key"];
-
-        // then
-        updated.Should().BeTrue();
-        value.Should().Be(43);
-    }
-
-    [Fact]
-    public void try_update_should_return_false_if_key_does_not_exist()
-    {
-        // given
-        var dictionary = new ConcurrentDictionary<string, int>(StringComparer.Ordinal);
-
-        // when
-        var updated = dictionary.TryUpdate("key", (key, oldValue) => oldValue + 1);
-
-        // then
-        updated.Should().BeFalse();
-    }
 }
