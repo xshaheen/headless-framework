c427cb97494ef68e4d0b1c7f5c08da0a292d804c
Shaheen <mxshaheen@gmail.com>
2025-01-14 15:03:42 +0300

    Add TimeSpan ToCancellationTokenSource ext

--- src/Framework.Base/Core/TimeSpanExtensions.cs
+++ src/Framework.Base/Core/TimeSpanExtensions.cs
@@ -15,4 +15,47 @@ public static class TimeSpanExtensions
             : timeSpan > max ? max
             : timeSpan;
     }
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static TimeSpan Min(this TimeSpan source, TimeSpan other)
+    {
+        return source.Ticks > other.Ticks ? other : source;
+    }
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static TimeSpan Max(this TimeSpan source, TimeSpan other)
+    {
+        return source.Ticks < other.Ticks ? other : source;
+    }
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan timeout)
+    {
+        if (timeout == TimeSpan.Zero)
+        {
+            var source = new CancellationTokenSource();
+            source.Cancel();
+
+            return source;
+        }
+
+        return timeout.Ticks > 0 ? new CancellationTokenSource(timeout) : new CancellationTokenSource();
+    }
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan? timeout)
+    {
+        return timeout.HasValue ? timeout.Value.ToCancellationTokenSource() : new CancellationTokenSource();
+    }
+
+    [SystemPure]
+    [JetBrainsPure]
+    public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan? timeout, TimeSpan defaultTimeout)
+    {
+        return (timeout ?? defaultTimeout).ToCancellationTokenSource();
+    }
 }
