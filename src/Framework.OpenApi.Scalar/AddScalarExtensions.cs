198ef86764f627a78ced6bf07edf51b270115e39
Shaheen <mxshaheen@gmail.com>
2025-01-18 01:32:02 +0200

    Upgrade packages

--- src/Framework.OpenApi.Scalar/AddScalarExtensions.cs
+++ src/Framework.OpenApi.Scalar/AddScalarExtensions.cs
@@ -9,40 +9,42 @@ public static class AddScalarExtensions
 {
     public static WebApplication MapFrameworkScalarOpenApi(this WebApplication app, Action<ScalarOptions> setupAction)
     {
-        app.MapScalarApiReference(options =>
-        {
-            options.EndpointPathPrefix = "/scalar/{documentName}";
-            options.OpenApiRoutePattern = "/openapi/{documentName}.json";
-            options.DarkMode = true;
-            options.HideDarkModeToggle = false;
-            options.Layout = ScalarLayout.Modern;
-            options.TagSorter = TagSorter.Alpha;
-            options.OperationSorter = OperationSorter.Method;
+        app.MapScalarApiReference(
+            endpointPrefix: "/scalar/{documentName}",
+            (options, _) =>
+            {
+                options.OpenApiRoutePattern = "/openapi/{documentName}.json";
+                options.DarkMode = true;
+                options.HideDarkModeToggle = false;
+                options.Layout = ScalarLayout.Modern;
+                options.TagSorter = TagSorter.Alpha;
+                options.OperationSorter = OperationSorter.Method;
 
-            options.EnabledTargets =
-            [
-                ScalarTarget.CSharp,
-                ScalarTarget.Go,
-                ScalarTarget.JavaScript,
-                ScalarTarget.Node,
-                ScalarTarget.PowerShell,
-                ScalarTarget.Shell,
-            ];
+                options.EnabledTargets =
+                [
+                    ScalarTarget.CSharp,
+                    ScalarTarget.Go,
+                    ScalarTarget.JavaScript,
+                    ScalarTarget.Node,
+                    ScalarTarget.PowerShell,
+                    ScalarTarget.Shell,
+                ];
 
-            options.EnabledClients =
-            [
-                ScalarClient.HttpClient,
-                ScalarClient.Curl,
-                ScalarClient.Axios,
-                ScalarClient.Fetch,
-                ScalarClient.Xhr,
-                ScalarClient.WebRequest,
-                ScalarClient.Wget,
-                ScalarClient.Httpie,
-            ];
+                options.EnabledClients =
+                [
+                    ScalarClient.HttpClient,
+                    ScalarClient.Curl,
+                    ScalarClient.Axios,
+                    ScalarClient.Fetch,
+                    ScalarClient.Xhr,
+                    ScalarClient.WebRequest,
+                    ScalarClient.Wget,
+                    ScalarClient.Httpie,
+                ];
 
-            setupAction(options);
-        });
+                setupAction(options);
+            }
+        );
 
         return app;
     }
