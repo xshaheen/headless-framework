ed4ecc3de47bd18997f7243a83b41ba22cf42d9f
Shaheen <mxshaheen@gmail.com>
2025-01-23 21:45:24 +0200

    Make userId complex type

--- src/Framework.BuildingBlocks/Primitives/AccountId.cs
+++ src/Framework.BuildingBlocks/Primitives/AccountId.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
+using System.ComponentModel.DataAnnotations.Schema;
 using Framework.Generator.Primitives;
 
 namespace Framework.Primitives;
@@ -7,6 +8,7 @@ namespace Framework.Primitives;
 /// <summary>Account identifier.</summary>
 /// <example>user-1234</example>
 [PublicAPI]
+[ComplexType]
 #pragma warning disable CA1036 // Override methods on comparable types
 public sealed partial class AccountId : IPrimitive<string>
 {
