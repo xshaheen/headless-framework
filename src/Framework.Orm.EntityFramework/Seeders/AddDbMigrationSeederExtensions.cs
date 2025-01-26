db8a9e04ed45caa72574de8b7c999be1c4a8dad4
Shaheen <mxshaheen@gmail.com>
2025-01-26 16:07:27 +0200

    Enhance ISeeder

---
+++ src/Framework.Orm.EntityFramework/Seeders/AddDbMigrationSeederExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Framework.Hosting.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.Orm.EntityFramework.Seeders;
+
+[PublicAPI]
+public static class AddDbMigrationSeederExtensions
+{
+    public static void AddDbMigrationSeeder<TContext>(this IServiceCollection services)
+        where TContext : DbContext
+    {
+        services.AddSeeder<DbMigrationSeeder<TContext>>();
+    }
+}
