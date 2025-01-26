640b6c64181565521b9da76b0ffcd5cd95b02480
Shaheen <mxshaheen@gmail.com>
2025-01-26 15:55:36 +0200

    Add DbMigrationSeeder.cs

---
+++ src/Framework.Orm.EntityFramework/Seeders/DbMigrationSeeder.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Framework.Hosting.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.Orm.EntityFramework.Seeders;
+
+public sealed class DbMigrationSeeder<TDbContext>(IServiceProvider provider) : ISeeder
+    where TDbContext : DbContext
+{
+    public async ValueTask SeedAsync()
+    {
+        await provider.MigrateDbContextAsync<TDbContext>();
+    }
+}
