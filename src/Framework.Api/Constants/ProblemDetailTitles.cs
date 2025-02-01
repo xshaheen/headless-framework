86fc6d01cc941091be86dcc422a17eaa4c0720e5
Shaheen <mxshaheen@gmail.com>
2025-02-01 17:45:10 +0200

    Refactor: update problem details handling for forbidden requests

--- src/Framework.Api/Constants/ProblemDetailTitles.cs
+++ src/Framework.Api/Constants/ProblemDetailTitles.cs
@@ -10,9 +10,7 @@ public static class ProblemDetailTitles
     public const string EndpointNotFounded = "endpoint-not-found";
     public const string EntityNotFounded = "entity-not-found";
     public const string ValidationProblem = "validation-problem";
-    public const string UnhandledException = "unhandled-exception";
     public const string BadRequest = "bad-request";
     public const string ConflictRequest = "conflict-request";
     public const string ForbiddenRequest = "forbidden-request";
-    public const string NotImplemented = "not-implemented";
 }
