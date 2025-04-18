adb013e25ca3be318bf7bc40308dadfcc152572a
Shaheen <mxshaheen@gmail.com>
2025-04-18 17:20:16 +0200

    Revamp DbMigrationSeeder to improve database migration handling with DbContext and DbContextFactory

--- src/Framework.Orm.EntityFramework/Seeders/DbMigrationSeeder.cs
+++ src/Framework.Orm.EntityFramework/Seeders/DbMigrationSeeder.cs
@@ -12,23 +12,31 @@ public sealed class DbMigrationSeeder<TDbContext>(IServiceProvider provider) : I
 {
     public async ValueTask SeedAsync()
     {
-        try
+        await using var scope = provider.CreateAsyncScope();
+
+        // Try with DbContext
+        var dbContext = scope.ServiceProvider.GetService<TDbContext>();
+
+        if (dbContext is not null)
         {
-            await provider.MigrateDbContextAsync<TDbContext>();
+            await dbContext.Database.MigrateAsync();
+
+            return;
         }
-        catch (Exception e)
+
+        // Try with DbContextFactory
+        var factory = scope.ServiceProvider.GetService<IDbContextFactory<TDbContext>>();
+
+        if (factory is not null)
         {
-            try
-            {
-                await provider.MigrateDbContextByFactoryAsync<TDbContext>();
-            }
-            catch
-            {
-                throw new InvalidOperationException(
-                    "Failed to migrate the database using both the DbContext and DbContextFactory methods.",
-                    e
-                );
-            }
+            await using var createdDbContext = await factory.CreateDbContextAsync();
+            await createdDbContext.Database.MigrateAsync();
+
+            return;
         }
+
+        throw new InvalidOperationException(
+            $"Unable to find {typeof(TDbContext).Name} or {typeof(IDbContextFactory<TDbContext>).Name} in the service provider."
+        );
     }
 }
