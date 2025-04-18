12d40f6a9a89862ffc1a44e88c77ba0c2666fd4c
Shaheen <mxshaheen@gmail.com>
2025-04-18 18:03:41 +0200

    Fix: json serializer

--- src/Framework.Serializer.Json/SystemJsonSerializer.cs
+++ src/Framework.Serializer.Json/SystemJsonSerializer.cs
@@ -11,19 +11,18 @@ public sealed class SystemJsonSerializer(JsonSerializerOptions? options = null) 
         return JsonSerializer.Deserialize<T>(data, _options);
     }
 
-    public void Serialize<T>(T value, Stream output)
-    {
-        using var writer = new Utf8JsonWriter(output);
-        JsonSerializer.Serialize(writer, value, _options);
-    }
-
     public object? Deserialize(Stream data, Type objectType)
     {
         return JsonSerializer.Deserialize(data, objectType, _options);
     }
 
+    public void Serialize<T>(T? value, Stream output)
+    {
+        JsonSerializer.Serialize(output, value, _options);
+    }
+
     public void Serialize(object? value, Stream output)
     {
-        JsonSerializer.Serialize(value, _options);
+        JsonSerializer.Serialize(output, value, value is null ? typeof(object) : value.GetType(), _options);
     }
 }
