f07d69b29af7a8b88c5bac7d49876659b65359ab
Shaheen <mxshaheen@gmail.com>
2025-04-21 02:21:56 +0200

    Add ActionContext extension methods for service resolution

---
+++ src/Framework.Api.Mvc/Extensions/ActionContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.Api.Mvc.Extensions;
+
+[PublicAPI]
+public static class ActionContextExtensions
+{
+    public static T GetRequiredService<T>(this FilterContext context)
+        where T : class
+    {
+        return context.HttpContext.RequestServices.GetRequiredService<T>();
+    }
+
+    public static T? GetService<T>(this FilterContext context, T? defaultValue = null)
+        where T : class
+    {
+        return context.HttpContext.RequestServices.GetService<T>() ?? defaultValue;
+    }
+}
