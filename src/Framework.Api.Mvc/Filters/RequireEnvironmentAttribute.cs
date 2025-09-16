f48fb5b28e01f9590eb85d07802cbaeb5a3e381d
Shaheen <mxshaheen@gmail.com>
2025-09-17 01:49:03 +0300

    Refactor RequireEnvironmentAttribute to rename environmentName to environment and update property usage

--- src/Framework.Api.Mvc/Filters/RequireEnvironmentAttribute.cs
+++ src/Framework.Api.Mvc/Filters/RequireEnvironmentAttribute.cs
@@ -11,16 +11,16 @@ namespace Framework.Api.Mvc.Filters;
 
 [PublicAPI]
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public sealed class RequireEnvironmentAttribute(string environmentName) : Attribute, IAsyncResourceFilter
+public sealed class RequireEnvironmentAttribute(string environment) : Attribute, IAsyncResourceFilter
 {
-    public string EnvironmentName { get; } = environmentName;
+    public string Environment { get; } = environment;
 
     public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
     {
         var services = context.HttpContext.RequestServices;
-        var environment = services.GetRequiredService<IWebHostEnvironment>();
+        var env = services.GetRequiredService<IWebHostEnvironment>();
 
-        if (environment.IsEnvironment(EnvironmentName))
+        if (env.IsEnvironment(Environment))
         {
             await next();
 
