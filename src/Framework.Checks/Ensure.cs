000554dbbbe41cfd7f39c04e9245b018aef5b9ff
Shaheen <mhshaheen@moj.gov.sa>
2025-09-21 19:33:48 +0300

    Add ReadOnlySequenceStream for exposing ReadOnlySequence<byte> as Stream with tests and runtime checks

--- src/Framework.Checks/Ensure.cs
+++ src/Framework.Checks/Ensure.cs
@@ -7,8 +7,10 @@ using Framework.Checks.Internals;
 
 namespace Framework.Checks;
 
-public static partial class Ensure
+/// <summary>Common runtime checks that throw exceptions upon failure.</summary>
+public static class Ensure
 {
+    /// <summary>Throws an <see cref="InvalidOperationException"/> if a condition is false.</summary>
     [DebuggerStepThrough]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void True(
@@ -17,27 +19,44 @@ public static partial class Ensure
         [CallerArgumentExpression(nameof(condition))] string? expression = null
     )
     {
-        if (condition)
+        if (!condition)
         {
-            return;
+            throw new InvalidOperationException(
+                message ?? $"The condition {expression.ToAssertString()} must be true."
+            );
         }
-
-        throw new InvalidOperationException(message ?? $"The condition {expression.ToAssertString()} must be true.");
     }
 
+    /// <summary>Throws an <see cref="InvalidOperationException"/> if a condition is true.</summary>
     [DebuggerStepThrough]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void False(
-        [DoesNotReturnIf(false)] bool condition,
+        [DoesNotReturnIf(true)] bool condition,
         string? message = null,
         [CallerArgumentExpression(nameof(condition))] string? expression = null
     )
     {
-        if (!condition)
+        if (condition)
         {
-            return;
+            throw new InvalidOperationException(
+                message ?? $"The condition {expression.ToAssertString()} must be false."
+            );
         }
+    }
 
-        throw new InvalidOperationException(message ?? $"The condition {expression.ToAssertString()} must be false.");
+    /// <summary>Throws an <see cref="ObjectDisposedException"/> if a condition is false.</summary>
+    [DebuggerStepThrough]
+    public static void NotDisposed([DoesNotReturnIf(true)] bool disposed, object? disposedValue, string? message = null)
+    {
+        if (disposed)
+        {
+            var objectName = disposedValue != null ? (disposedValue.GetType().FullName ?? string.Empty) : string.Empty;
+            if (message != null)
+            {
+                throw new ObjectDisposedException(objectName, message);
+            }
+
+            throw new ObjectDisposedException(objectName);
+        }
     }
 }
