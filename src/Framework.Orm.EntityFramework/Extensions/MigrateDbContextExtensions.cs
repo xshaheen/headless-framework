097031f35bf97aea23f73a0308f1838038fb16d4
Shaheen <mxshaheen@gmail.com>
2025-10-04 21:07:08 +0300

    feat: add methods for database migration and creation in DbContext extensions

--- src/Framework.Orm.EntityFramework/Extensions/MigrateDbContextExtensions.cs
+++ src/Framework.Orm.EntityFramework/Extensions/MigrateDbContextExtensions.cs
@@ -9,6 +9,40 @@ namespace Microsoft.Extensions.DependencyInjection;
 [PublicAPI]
 public static class MigrateDbContextExtensions
 {
+    public static void MigrateDbContext<TContext>(this IServiceProvider services)
+        where TContext : DbContext
+    {
+        using var scope = services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        context.Database.Migrate();
+    }
+
+    public static void MigrateDbContextByFactory<TContext>(this IServiceProvider services)
+        where TContext : DbContext
+    {
+        using var scope = services.CreateScope();
+        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+        using var context = factory.CreateDbContext();
+        context.Database.Migrate();
+    }
+
+    public static void EnsureDbCreated<TContext>(this IServiceProvider services)
+        where TContext : DbContext
+    {
+        using var scope = services.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        context.Database.EnsureCreated();
+    }
+
+    public static void EnsureDbCreatedByFactory<TContext>(this IServiceProvider services)
+        where TContext : DbContext
+    {
+        using var scope = services.CreateScope();
+        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+        using var context = factory.CreateDbContext();
+        context.Database.EnsureCreated();
+    }
+
     public static async Task MigrateDbContextAsync<TContext>(
         this IServiceProvider services,
         CancellationToken token = default
