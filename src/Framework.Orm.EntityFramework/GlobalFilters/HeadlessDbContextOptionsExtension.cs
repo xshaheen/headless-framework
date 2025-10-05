3618c1735539b73c98d0522674697a5c4adfafce
Shaheen <mxshaheen@gmail.com>
2025-10-06 01:52:47 +0300

    refactor: simplify HeadlessDbContextOptionsExtension and extract service registration logic

--- src/Framework.Orm.EntityFramework/GlobalFilters/HeadlessDbContextOptionsExtension.cs
+++ src/Framework.Orm.EntityFramework/GlobalFilters/HeadlessDbContextOptionsExtension.cs
@@ -1,45 +1,14 @@
 ﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
 
-using Framework.Abstractions;
-using Framework.Orm.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Framework.Orm.EntityFramework.GlobalFilters;
 
 /// <inheritdoc />
 public sealed class HeadlessDbContextOptionsExtension : IDbContextOptionsExtension
 {
-    public void ApplyServices(IServiceCollection services)
-    {
-        services.TryAddSingleton<IHeadlessEntityModelProcessor, HeadlessEntityModelProcessor>();
-        services.TryAddSingleton<IClock, Clock>();
-        services.TryAddSingleton<IGuidGenerator, SequentialAtEndGuidGenerator>();
-        services.TryAddSingleton<ICurrentTenant, NullCurrentTenant>();
-        services.TryAddSingleton<ICurrentUser, NullCurrentUser>();
-
-        var descriptor = services.FirstOrDefault(x => x.ServiceType == typeof(ICompiledQueryCacheKeyGenerator));
-
-        if (descriptor is { ImplementationType: not null })
-        {
-            services.Remove(descriptor);
-            services.AddScoped(descriptor.ImplementationType);
-
-            services.Add(
-                ServiceDescriptor.Scoped<ICompiledQueryCacheKeyGenerator>(provider =>
-                {
-                    var generator = ActivatorUtilities.CreateInstance<HeadlessCompiledQueryCacheKeyGenerator>(
-                        provider,
-                        (ICompiledQueryCacheKeyGenerator)provider.GetRequiredService(descriptor.ImplementationType)
-                    );
-
-                    return generator;
-                })
-            );
-        }
-    }
+    public void ApplyServices(IServiceCollection services) => services.AddHeadlessDbContextServices();
 
     public void Validate(IDbContextOptions options) { }
 
@@ -53,10 +22,7 @@ public sealed class HeadlessDbContextOptionsExtension : IDbContextOptionsExtensi
 
         public override void PopulateDebugInfo(IDictionary<string, string> debugInfo) { }
 
-        public override int GetServiceProviderHashCode()
-        {
-            return 0;
-        }
+        public override int GetServiceProviderHashCode() => 0;
 
         public override bool ShouldUseSameServiceProvider(DbContextOptionsExtensionInfo other)
         {
