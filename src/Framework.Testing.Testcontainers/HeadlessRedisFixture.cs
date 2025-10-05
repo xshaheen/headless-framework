21dfa59a8cf1960fe4dbab4dcfc41a9a4e852afd
Shaheen <mhshaheen@moj.gov.sa>
2025-10-05 23:30:49 +0300

    feat: add retry functionality for Xunit tests with custom attributes and message bus

---
+++ src/Framework.Testing.Testcontainers/HeadlessRedisFixture.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Mahmoud Shaheen. All rights reserved.
+
+using Testcontainers.Redis;
+using Testcontainers.Xunit;
+
+namespace Framework.Testing.Testcontainers;
+
+public class HeadlessRedisFixture() : ContainerFixture<RedisBuilder, RedisContainer>(TestContextMessageSink.Instance)
+{
+    protected override RedisBuilder Configure(RedisBuilder builder)
+    {
+        return base.Configure(builder).WithImage("redis:7.4");
+    }
+}
