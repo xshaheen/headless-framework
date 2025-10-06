3d5b92930e29579b0e58e78b2fb8e170bc1ca494
Shaheen <mhshaheen@moj.gov.sa>
2025-10-06 08:33:54 +0300

    refactor: enhance MigrateDbContextExtensions with async methods and additional database management functions

--- src/Framework.Orm.EntityFramework/Extensions/MigrateDbContextExtensions.cs
+++ src/Framework.Orm.EntityFramework/Extensions/MigrateDbContextExtensions.cs
@@ -9,83 +9,130 @@ namespace Microsoft.Extensions.DependencyInjection;
 [PublicAPI]
 public static class MigrateDbContextExtensions
 {
-    public static void MigrateDbContext<TContext>(this IServiceProvider services)
-        where TContext : DbContext
+    extension<TContext>(IServiceProvider services) where TContext : DbContext
     {
-        using var scope = services.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<TContext>();
-        context.Database.Migrate();
-    }
+        public void MigrateDbContext()
+        {
+            using var scope = services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            context.Database.Migrate();
+        }
 
-    public static void MigrateDbContextByFactory<TContext>(this IServiceProvider services)
-        where TContext : DbContext
-    {
-        using var scope = services.CreateScope();
-        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
-        using var context = factory.CreateDbContext();
-        context.Database.Migrate();
-    }
+        public async Task MigrateDbContextAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            await context.Database.MigrateAsync(token);
+        }
 
-    public static void EnsureDbCreated<TContext>(this IServiceProvider services)
-        where TContext : DbContext
-    {
-        using var scope = services.CreateAsyncScope();
-        var context = scope.ServiceProvider.GetRequiredService<TContext>();
-        context.Database.EnsureCreated();
-    }
+        public void MigrateDbContextByFactory()
+        {
+            using var scope = services.CreateScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            using var context = factory.CreateDbContext();
+            context.Database.Migrate();
+        }
 
-    public static void EnsureDbCreatedByFactory<TContext>(this IServiceProvider services)
-        where TContext : DbContext
-    {
-        using var scope = services.CreateScope();
-        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
-        using var context = factory.CreateDbContext();
-        context.Database.EnsureCreated();
-    }
+        public async Task MigrateDbContextByFactoryAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            await using var context = await factory.CreateDbContextAsync(token);
+            await context.Database.MigrateAsync(token);
+        }
 
-    public static async Task MigrateDbContextAsync<TContext>(
-        this IServiceProvider services,
-        CancellationToken token = default
-    )
-        where TContext : DbContext
-    {
-        await using var scope = services.CreateAsyncScope();
-        var context = scope.ServiceProvider.GetRequiredService<TContext>();
-        await context.Database.MigrateAsync(token);
-    }
+        public void EnsureDbCreated()
+        {
+            using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            context.Database.EnsureCreated();
+        }
 
-    public static async Task MigrateDbContextByFactoryAsync<TContext>(
-        this IServiceProvider services,
-        CancellationToken token = default
-    )
-        where TContext : DbContext
-    {
-        await using var scope = services.CreateAsyncScope();
-        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
-        await using var context = await factory.CreateDbContextAsync(token);
-        await context.Database.MigrateAsync(token);
-    }
+        public async Task EnsureDbCreatedAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            await context.Database.EnsureCreatedAsync(token);
+        }
 
-    public static async Task EnsureDbCreatedAsync<TContext>(
-        this IServiceProvider services,
-        CancellationToken token = default
-    )
-        where TContext : DbContext
-    {
-        await using var scope = services.CreateAsyncScope();
-        var context = scope.ServiceProvider.GetRequiredService<TContext>();
-        await context.Database.EnsureCreatedAsync(token);
-    }
+        public void EnsureDbCreatedByFactory()
+        {
+            using var scope = services.CreateScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            using var context = factory.CreateDbContext();
+            context.Database.EnsureCreated();
+        }
 
-    public static async Task EnsureDbCreatedByFactoryAsync<TContext>(
-        this IServiceProvider services,
-        CancellationToken token = default
-    )
-        where TContext : DbContext
-    {
-        await using var scope = services.CreateAsyncScope();
-        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
-        await using var context = await factory.CreateDbContextAsync(token);
-        await context.Database.EnsureCreatedAsync(token);
+        public async Task EnsureDbCreatedByFactoryAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            await using var context = await factory.CreateDbContextAsync(token);
+            await context.Database.EnsureCreatedAsync(token);
+        }
+
+        public void EnsureDbDeleted()
+        {
+            using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            context.Database.EnsureDeleted();
+        }
+
+        public async Task EnsureDbDeletedAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            await context.Database.EnsureDeletedAsync(token);
+        }
+
+        public void EnsureDbDeletedByFactory()
+        {
+            using var scope = services.CreateScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            using var context = factory.CreateDbContext();
+            context.Database.EnsureDeleted();
+        }
+
+        public async Task EnsureDbDeletedByFactoryAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            await using var context = await factory.CreateDbContextAsync(token);
+            await context.Database.EnsureDeletedAsync(token);
+        }
+
+        public void EnsureDbRecreated()
+        {
+            using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        public async Task EnsureDbRecreatedAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+            await context.Database.EnsureDeletedAsync(token);
+            await context.Database.EnsureCreatedAsync(token);
+        }
+
+        public void EnsureDbRecreatedByFactory()
+        {
+            using var scope = services.CreateScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            using var context = factory.CreateDbContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        public async Task EnsureDbRecreatedByFactoryAsync(CancellationToken token = default)
+        {
+            await using var scope = services.CreateAsyncScope();
+            var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<TContext>>();
+            await using var context = await factory.CreateDbContextAsync(token);
+            await context.Database.EnsureDeletedAsync(token);
+            await context.Database.EnsureCreatedAsync(token);
+        }
     }
 }
