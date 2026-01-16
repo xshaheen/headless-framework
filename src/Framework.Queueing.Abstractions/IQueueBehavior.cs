5b763b9eb694fe27f1ee4a833a8f1e92b809a18f
Shaheen <mxshaheen@gmail.com>
2026-01-16 19:24:25 +0200

    chore(solution): remove Framework.Queueing project from solution file

--- src/Framework.Queueing.Abstractions/IQueueBehavior.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mahmoud Shaheen. All rights reserved.
-
-namespace Framework.Queueing;
-
-public interface IQueueBehavior<T>
-    where T : class
-{
-    void Attach(IQueue<T> queue);
-}
