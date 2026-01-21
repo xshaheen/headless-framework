2573594b485a420f51c7ee40a16f6d52526f80ad
Shaheen <mxshaheen@gmail.com>
2026-01-21 15:51:14 +0200

    refactor: add generic PrimitiveValueConverter for IPrimitive types
    - Created PrimitiveValueConverter<TPrimitive, TValue> base class
    - Refactored UserIdValueConverter and AccountIdValueConverter to use generic base
    - Removed TODO comment
    - Eliminates code duplication across primitive converters

--- src/Framework.Orm.EntityFramework/Configurations/UserIdValueConverter.cs
+++ src/Framework.Orm.EntityFramework/Configurations/UserIdValueConverter.cs
@@ -5,13 +5,12 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Framework.Orm.EntityFramework.Configurations;
 
-// TODO: Consider use generic type converter for IPrimitive types
 /// <summary>ValueConverter for <see cref = "UserId"/></summary>
-public sealed class UserIdValueConverter : ValueConverter<UserId, string>
+public sealed class UserIdValueConverter : PrimitiveValueConverter<UserId, string>
 {
     public UserIdValueConverter()
-        : base(v => v, v => v) { }
+        : base() { }
 
-    public UserIdValueConverter(ConverterMappingHints? mappingHints = null)
-        : base(v => v, v => v, mappingHints) { }
+    public UserIdValueConverter(ConverterMappingHints? mappingHints)
+        : base(mappingHints) { }
 }
