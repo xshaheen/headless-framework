9eecf8de76ffa634fc4ff873ab6b880503d00077
Shaheen <mxshaheen@gmail.com>
2026-01-25 12:41:47 +0200

    test(slug): add comprehensive tests for Slug.Create and SlugOptions functionality

--- tests/Framework.Slugs.Tests.Unit/SlugTests.cs
+++ tests/Framework.Slugs.Tests.Unit/SlugTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
+using System.Text.Unicode;
 using Framework.Slugs;
 
 namespace Tests;
@@ -138,4 +139,268 @@ public sealed class SlugTests
         // Mathematical bold A (U+1D400)
         Slug.Create("test \U0001D400 math").Should().Be("test-math");
     }
+
+    #region Slug.Create - Edge Cases
+
+    [Fact]
+    public void should_use_default_options_when_null()
+    {
+        Slug.Create("Hello World", options: null).Should().Be("hello-world");
+    }
+
+    [Fact]
+    public void should_handle_very_long_input()
+    {
+        var longInput = new string('a', 1000);
+        var result = Slug.Create(longInput);
+
+        result.Should().HaveLength(SlugOptions.DefaultMaximumLength);
+        result.Should().Be(new string('a', 80));
+    }
+
+    [Theory]
+    [InlineData("hello---world", "hello-world")]
+    [InlineData("hello------world", "hello-world")]
+    public void should_handle_consecutive_separators_in_input(string input, string expected)
+    {
+        Slug.Create(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("---hello", "hello")]
+    [InlineData("   hello", "hello")]
+    [InlineData("-hello", "hello")]
+    public void should_not_start_with_separator(string input, string expected)
+    {
+        Slug.Create(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("!@#$%^*()", "percent")]
+    [InlineData("!@#^*()", "")]
+    public void should_handle_only_special_characters(string input, string expected)
+    {
+        // % gets replaced with " percent " by default replacements
+        var options = new SlugOptions { MaximumLength = 0 };
+        Slug.Create(input, options).Should().Be(expected);
+    }
+
+    [Fact]
+    public void should_handle_mixed_unicode_scripts()
+    {
+        var options = new SlugOptions { MaximumLength = 0 };
+        Slug.Create("hello مرحبا world", options).Should().Be("hello-مرحبا-world");
+    }
+
+    #endregion
+
+    #region SlugOptions - AllowedRanges
+
+    [Fact]
+    public void should_allow_all_characters_when_ranges_empty()
+    {
+        var options = new SlugOptions { AllowedRanges = [], MaximumLength = 0 };
+
+        // With empty ranges, all chars including special should pass through
+        Slug.Create("hello!world", options).Should().Be("hello!world");
+    }
+
+    [Fact]
+    public void should_filter_by_custom_unicode_range()
+    {
+        var options = new SlugOptions
+        {
+            AllowedRanges = [UnicodeRange.Create('a', 'f')], // only a-f allowed
+            MaximumLength = 0,
+        };
+
+        Slug.Create("abcdefghij", options).Should().Be("abcdef");
+    }
+
+    [Fact]
+    public void should_allow_arabic_characters_by_default()
+    {
+        // Arabic range U+0620-U+064A is in default AllowedRanges
+        Slug.Create("مرحبا").Should().Be("مرحبا");
+    }
+
+    [Theory]
+    [InlineData("test123", "test123")]
+    [InlineData("0123456789", "0123456789")]
+    public void should_allow_digits_by_default(string input, string expected)
+    {
+        Slug.Create(input).Should().Be(expected);
+    }
+
+    #endregion
+
+    #region SlugOptions - Culture
+
+    [Fact]
+    public void should_use_culture_for_lowercase()
+    {
+        // Turkish: uppercase I becomes dotless ı (U+0131) in lowercase
+        var options = new SlugOptions
+        {
+            Culture = new CultureInfo("tr-TR"),
+            CasingTransformation = CasingTransformation.ToLowerCase,
+        };
+
+        Slug.Create("HI", options).Should().Be("hı");
+    }
+
+    [Fact]
+    public void should_use_culture_for_uppercase()
+    {
+        // Turkish: lowercase i becomes İ (U+0130) in uppercase
+        var options = new SlugOptions
+        {
+            Culture = new CultureInfo("tr-TR"),
+            CasingTransformation = CasingTransformation.ToUpperCase,
+        };
+
+        Slug.Create("hi", options).Should().Be("Hİ");
+    }
+
+    [Fact]
+    public void should_use_invariant_when_culture_null()
+    {
+        // With null culture, I becomes regular lowercase i
+        var options = new SlugOptions { Culture = null, CasingTransformation = CasingTransformation.ToLowerCase };
+
+        Slug.Create("HI", options).Should().Be("hi");
+    }
+
+    #endregion
+
+    #region SlugOptions - Replacements
+
+    [Theory]
+    [InlineData("rock & roll", "rock-and-roll")]
+    [InlineData("C++", "c-plus-plus")]
+    [InlineData(".NET", "dot-net")]
+    [InlineData("100%", "100-percent")]
+    public void should_apply_default_replacements(string input, string expected)
+    {
+        var options = new SlugOptions { MaximumLength = 0 };
+        Slug.Create(input, options).Should().Be(expected);
+    }
+
+    [Fact]
+    public void should_apply_custom_replacements()
+    {
+        var options = new SlugOptions
+        {
+            Replacements = new Dictionary<string, string> { { "@", " at " } }.ToFrozenDictionary(),
+            MaximumLength = 0,
+        };
+
+        Slug.Create("user@domain", options).Should().Be("user-at-domain");
+    }
+
+    [Fact]
+    public void should_handle_empty_replacements()
+    {
+        var options = new SlugOptions { Replacements = FrozenDictionary<string, string>.Empty, MaximumLength = 0 };
+
+        // & won't be replaced, will be filtered as disallowed character
+        Slug.Create("rock & roll", options).Should().Be("rock-roll");
+    }
+
+    [Fact]
+    public void should_apply_replacements_before_filtering()
+    {
+        // Replacements happen first, then filtering by AllowedRanges
+        var options = new SlugOptions
+        {
+            Replacements = new Dictionary<string, string> { { "&", "AND" } }.ToFrozenDictionary(),
+            MaximumLength = 0,
+        };
+
+        // & replaced with AND, then lowercased
+        Slug.Create("A&B", options).Should().Be("aandb");
+    }
+
+    #endregion
+
+    #region SlugOptions - MaximumLength Edge Cases
+
+    [Fact]
+    public void should_not_truncate_when_max_is_zero()
+    {
+        var options = new SlugOptions { MaximumLength = 0 };
+        Slug.Create("hello world this is a long string", options).Should().Be("hello-world-this-is-a-long-string");
+    }
+
+    [Fact]
+    public void should_truncate_to_exact_max()
+    {
+        var options = new SlugOptions { MaximumLength = 5 };
+        Slug.Create("hello-world", options).Should().Be("hello");
+    }
+
+    [Fact]
+    public void should_remove_trailing_separator_after_truncation()
+    {
+        // "hello-world" truncated to 6 chars = "hello-", then trailing "-" removed
+        var options = new SlugOptions { MaximumLength = 6 };
+        Slug.Create("hello world", options).Should().Be("hello");
+    }
+
+    #endregion
+
+    #region SlugOptions - IsAllowed Method
+
+    [Fact]
+    public void should_return_true_for_allowed_rune()
+    {
+        var options = new SlugOptions();
+        options.IsAllowed(new Rune('a')).Should().BeTrue();
+        options.IsAllowed(new Rune('Z')).Should().BeTrue();
+        options.IsAllowed(new Rune('5')).Should().BeTrue();
+    }
+
+    [Fact]
+    public void should_return_false_for_disallowed_rune()
+    {
+        var options = new SlugOptions();
+        options.IsAllowed(new Rune('!')).Should().BeFalse();
+        options.IsAllowed(new Rune('@')).Should().BeFalse();
+        options.IsAllowed(new Rune(' ')).Should().BeFalse();
+    }
+
+    [Fact]
+    public void should_handle_boundary_values()
+    {
+        var options = new SlugOptions { AllowedRanges = [UnicodeRange.Create('a', 'z')] };
+
+        // Boundary: first char 'a' and last char 'z' should be allowed
+        options.IsAllowed(new Rune('a')).Should().BeTrue();
+        options.IsAllowed(new Rune('z')).Should().BeTrue();
+
+        // Just outside: '`' (before 'a') and '{' (after 'z') should be disallowed
+        options.IsAllowed(new Rune('`')).Should().BeFalse();
+        options.IsAllowed(new Rune('{')).Should().BeFalse();
+    }
+
+    #endregion
+
+    #region SlugOptions - Multi-char Separator
+
+    [Fact]
+    public void should_support_multi_char_separator()
+    {
+        var options = new SlugOptions { Separator = "__" };
+        Slug.Create("hello world", options).Should().Be("hello__world");
+    }
+
+    [Fact]
+    public void should_not_duplicate_multi_char_separator()
+    {
+        var options = new SlugOptions { Separator = "__", MaximumLength = 0 };
+        // Multiple spaces should not result in "____"
+        Slug.Create("hello   world", options).Should().Be("hello__world");
+    }
+
+    #endregion
 }
