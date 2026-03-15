26087323e88f6f010d73b0f39aa719fa09768008
Shaheen <mxshaheen@gmail.com>
2026-03-15 11:35:25 +0200

    fix(jobs): correct friend assembly names

--- src/Headless.Jobs.Abstractions/Properties/AssemblyInfo.cs
+++ src/Headless.Jobs.Abstractions/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Jobs")]
-[assembly: InternalsVisibleTo("Headless.Jobs.EntityFrameworkCore")]
+[assembly: InternalsVisibleTo("Headless.Jobs.Core")]
+[assembly: InternalsVisibleTo("Headless.Jobs.EntityFramework")]
 [assembly: InternalsVisibleTo("Headless.Jobs.Dashboard")]
 [assembly: InternalsVisibleTo("Headless.Jobs.Tests.Unit")]
-[assembly: InternalsVisibleTo("Headless.Jobs.Instrumentation.OpenTelemetry")]
-[assembly: InternalsVisibleTo("Headless.Jobs.Caching.StackExchangeRedis")]
+[assembly: InternalsVisibleTo("Headless.Jobs.OpenTelemetry")]
+[assembly: InternalsVisibleTo("Headless.Jobs.Caching.Redis")]
 // To be testable using NSubsitute
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
