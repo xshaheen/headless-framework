a197e9d2915dc37c4bfa04de04c7394d11e3aca6
Shaheen <mxshaheen@gmail.com>
2026-05-11 12:50:23 +0300

    chore(tests): add missing using directives for renamed namespace

--- tests/Headless.Orm.Tests.Harness/Tests/HeadlessDbContextGlobalFiltersTestBase.cs
+++ tests/Headless.Orm.Tests.Harness/Tests/HeadlessDbContextGlobalFiltersTestBase.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Mahmoud Shaheen. All rights reserved.
 
+using Headless.EntityFramework;
 using Headless.EntityFramework.Contexts;
 using Headless.Testing.Tests;
 using Microsoft.EntityFrameworkCore;
