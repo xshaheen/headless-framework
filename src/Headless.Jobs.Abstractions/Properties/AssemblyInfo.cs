a01d65cba2b08afcb6f27d144d9347ffc38c40b9
Shaheen <mxshaheen@gmail.com>
2026-06-10 01:51:17 +0300

    refactor(jobs): declare InternalsVisibleTo in csproj, drop Properties assembly-info files

--- src/Headless.Jobs.Abstractions/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Headless.Jobs.Core")]
-[assembly: InternalsVisibleTo("Headless.Jobs.EntityFramework")]
-[assembly: InternalsVisibleTo("Headless.Jobs.Dashboard")]
-[assembly: InternalsVisibleTo("Headless.Jobs.Tests.Unit")]
-[assembly: InternalsVisibleTo("Headless.Jobs.OpenTelemetry")]
-// To be testable using NSubsitute
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
